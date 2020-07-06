3c65ed248cb52ac412ec6bb43aa3d4da084ebab0
Askarjon Abdullayev <39943207+askarjon111@users.noreply.github.com>
2020-07-06 21:45:58 +0500

    Revert "Code formatlari teahlanib chiqildi"
    This reverts commit 9da0dcacb16fb75cf1a951bdcca9871986a79fb0.

--- manage.py
+++ manage.py
@@ -5,11 +5,10 @@ import sys
 
 
 def main():
-                    os.environ.setdefault(
-                        'DJANGO_SETTINGS_MODULE', 'test_system.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'test_system.settings')
     try:
         from django.core.management import execute_from_command_line
-    except              ImportError              as exc:
+    except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
