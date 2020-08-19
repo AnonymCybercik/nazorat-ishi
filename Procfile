c4feed38f0080fbc3bbed7209f260ee2f2aea889
Doniyor <xoja9636706@gmail.com>
2020-08-19 21:57:44 +0500

    ch(deploy):Deploy app

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn TEST_SYSTEM.wsgi
--- test_system/settings.py
+++ test_system/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os,sys
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -135,3 +136,5 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT,'assets')
 MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'media')
 
 MEDIA_URL = '/media/'
+
+django_heroku.settings(locals())
