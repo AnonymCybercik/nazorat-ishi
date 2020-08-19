a94f71b3e3b1ea10bfefb0258fc2eb85138b2ac3
Doniyor <xoja9636706@gmail.com>
2020-08-19 20:07:48 +0500

    assets

--- test_system/settings.py
+++ test_system/settings.py
@@ -126,12 +126,12 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'static')
+    os.path.join(PROJECT_ROOT,'static')
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR,'assets')
+STATIC_ROOT = os.path.join(PROJECT_ROOT,'assets')
 
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'media')
 
 MEDIA_URL = '/media/'
