1d9a34fb87df9502b636e48ae466c17998efc82d
Doniyor <60249713+AnonymCybercik@users.noreply.github.com>
2020-09-14 23:40:21 +0500

    Update urls.py

--- test_system/urls.py
+++ test_system/urls.py
@@ -2,8 +2,7 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf.urls import url,handler404,handler500
 
-handler404 = 'my_tests.views.views_404'
-handler500 = 'my_tests.views.views_404'
+
 
 urlpatterns = [
     path('',include('my_tests.urls')),
